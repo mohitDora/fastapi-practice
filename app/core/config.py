17ecc0bb7c46c1d75bf2c9d063cdc35b977315ff
Mohit-Gyansys <doramohitkumar@gmail.com>
2025-09-05 12:13:54 +0530

    config done

---
+++ app/core/config.py
@@ -0,0 +1,45 @@
+from pydantic import PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    # BaseSettings gives ability class to read from .env and automatically map them to class attributes
+
+    # env_ignore_empty=True : Tells pydantic to ignore any ennv variables that are empty
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = "demosecretkey"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
+
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "password"
+    POSTGRES_DB: str = "todo_app"
+
+    # @property : turns a method into an attribute, means now we can call the fucntion w/o using the parenthesis.
+    @computed_field
+    @property
+    def DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+    BACKEND_CORS_ORIGINS: list[str] = [
+        "http://localhost",
+        "http://localhost:3000",
+        "http://localhost:8080",
+    ]
+    
+    FIRST_SUPERUSER_EMAIL: str = "admin@example.com"
+    FIRST_SUPERUSER_PASSWORD: str = "changethis"
+
+Settings = Settings()
