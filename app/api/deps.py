821f24654269eed5ebdb78c86bf09b63fa63c3d3
Mohit-Gyansys <doramohitkumar@gmail.com>
2025-09-06 20:53:14 +0530

    deps done

---
+++ app/api/deps.py
@@ -0,0 +1,54 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+from jose import jwt, JWTError
+
+from app.core.config import settings
+from app.core.db import get_session
+from app.core import security
+from app.crud import user as crud_user
+from app.models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login")
+
+SeesionDep = Annotated[Session, Depends(get_session)]
+TokenDep = Annotated[str, Depends(oauth2_scheme)]
+
+
+async def get_current_user(token: TokenDep, session: SeesionDep):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, security.ALGORITHM)
+        token_data = payload.get("sub")
+        if token_data is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = crud_user.user.get(session, id=token_data)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+def get_current_active_user(current_user: CurrentUser):
+    if not crud_user.user.is_active(current_user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+def get_current_active_superuser(current_user: CurrentUser):
+    if not crud_user.user.is_superuser(current_user):
+        raise HTTPException(
+            status_code=400, detail="The user doesn't have enough privileges"
+        )
+    return current_user
